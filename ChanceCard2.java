a6a2787de7405cc6c27e8f24023471a21ccdae2e
YiqingHu <yiqinghu.mckinley259@gmail.com>
2016-06-01 00:24:04 -0400

    renaming
    num. starts with 0

--- ChanceCard2.java
+++ ChanceCard2.java
@@ -1,15 +1,15 @@
 public class ChanceCard2 implements ChanceCard{
 
     private final int _id=2;
-    private final String _text = "Advance to Illinois Avenue. - If you pass Go, collect $200."
+    private final String _text = "Advance to St. Charles Place. - If you pass Go, collect $200."
 
     public int getId(){
       return _id;
     }
 
     public void action(Player p){
-      //if (p.getPosition() > 24) p.addMoney(200);
+      //if (p.getPosition() > 11) p.addMoney(200);
       //it would be cool if position has a number...
-      p.setPosition(board.getSpace(24));
+      p.setPosition(board.getSpace(11));
     }
 }
