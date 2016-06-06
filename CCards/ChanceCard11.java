6b2225423a5947724436bdc91148f2fe2cd3ad20
YiqingHu <yiqinghu.mckinley259@gmail.com>
2016-06-06 09:51:33 -0400

    reading railroad done

--- CCards/ChanceCard11.java
+++ CCards/ChanceCard11.java
@@ -1,15 +1,14 @@
 public class ChanceCard11 implements ChanceCard{
 
     private final int _id=11;
-    private final String _text = "Take a trip to Reading Railroad ¨C If you pass Go, collect $200"
+    private final String _text = "Take a trip to Reading Railroad Â¨C If you pass Go, collect $200"
 
     public int getId(){
       return _id;
     }
 
     public void action(Player p){
-      //if (p.getPosition() > 24) p.addMoney(200);
-      //it would be cool if position has a number...
-      p.setPosition(board.getSpace(24));
+      if (p.getPosition() > 5) p.addMoney(200);
+      p.setPosition(board.getSpace(5));
     }
 }
