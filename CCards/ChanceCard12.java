2c31a06beb86e983a73ae29580408188992dfe70
YiqingHu <yiqinghu.mckinley259@gmail.com>
2016-06-06 09:51:57 -0400

    broadwalk done

--- CCards/ChanceCard12.java
+++ CCards/ChanceCard12.java
@@ -1,15 +1,13 @@
 public class ChanceCard12 implements ChanceCard{
 
     private final int _id=12;
-    private final String _text = "Take a walk on the Boardwalk ¨C Advance token to Boardwalk "
+    private final String _text = "Take a walk on the Boardwalk Â¨C Advance token to Boardwalk "
 
     public int getId(){
       return _id;
     }
 
     public void action(Player p){
-      //if (p.getPosition() > 24) p.addMoney(200);
-      //it would be cool if position has a number...
-      p.setPosition(board.getSpace(24));
+      p.setPosition(board.getSpace(39));
     }
 }
