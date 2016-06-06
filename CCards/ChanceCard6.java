9c4e41a5f9750a9f0a64e4037b3a90598fc727f9
YiqingHu <yiqinghu.mckinley259@gmail.com>
2016-06-06 09:45:18 -0400

    Get out of jail

--- CCards/ChanceCard6.java
+++ CCards/ChanceCard6.java
@@ -1,12 +1,13 @@
 public class ChanceCard6 implements ChanceCard{
 
     private final int _id=6;
-    private final String _text = "Get out of Jail Free ¨C This card may be kept until needed, or traded/sold" 
+    private final String _text = "Get out of Jail Free Â¨C This card may be kept until needed, or traded/sold" 
 
     public int getId(){
       return _id;
     }
 
     public void action(Player p){
+        //you hold it on your hand
     }
 }
