49c633b33461e5b4ba1638f3c69edc431002229b
YiqingHu <yiqinghu.mckinley259@gmail.com>
2016-06-06 09:48:48 -0400

    go to jail done

--- CCards/ChanceCard8.java
+++ CCards/ChanceCard8.java
@@ -1,15 +1,13 @@
 public class ChanceCard8 implements ChanceCard{
 
     private final int _id=8;
-    private final String _text = "Go to Jail ¨C Go directly to Jail ¨C Do not pass Go, do not collect $200"
+    private final String _text = "Go to Jail Â¨C Go directly to Jail Â¨C Do not pass Go, do not collect $200"
 
     public int getId(){
       return _id;
     }
 
     public void action(Player p){
-      //if (p.getPosition() > 24) p.addMoney(200);
-      //it would be cool if position has a number...
       p.setPosition(board.getSpace(24));
     }
 }
