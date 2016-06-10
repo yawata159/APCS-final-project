e22577d131568f336fcc69190dd4bba91bcdd04b
plwslw <s.lit.winkler@gmail.com>
2016-06-10 10:15:48 -0400

    7

---
+++ ChanceCard7.java
@@ -0,0 +1,13 @@
+public class ChanceCard7 implements ChanceCard{
+
+    private final int _id=7;
+    private final String _text = "Go Back 3 Spaces."
+
+    public int getId(){
+      return _id;
+    }
+
+    public void action(Player p){
+        p.setPosition(board.getSpace(p.getIntPosition() - 3));
+    }
+}
